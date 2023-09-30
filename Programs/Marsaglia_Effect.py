1f3f47671afa0fe2d46f603407278a9cb6031728
Juan Pablo Castro <j.idarragam@uniandes.edu.co>
2023-09-30 16:06:38 +0200

    Good coding

--- Programs/Marsaglia_Effect.py
+++ Programs/Marsaglia_Effect.py
@@ -10,10 +10,8 @@ from matplotlib import style
 N = 1200
 x=np.zeros(N)
 num =np.zeros(N)
-print(num)
 
 x[0] = 9283983
-
 for i in range(0,N-1):
     x[i+1] = (65539*x[i])%(2**31)
     num[i+1] = x[i+1]/2**31
@@ -21,30 +19,12 @@ for i in range(0,N-1):
 num[0] = num[-1]
 
 
-
-
-
-u=np.zeros(N)
-u2=np.zeros(N)
-u3=np.zeros(N)
-print(u)
-u[0:N] =num[0:N]
-u2[0:N-1] =num[1:N] 
-u3[0:N-2] =num[2:N]
-u2[N-1] = u[0]
-u3[N-2] = u[0]
-u3[N-1] = u[1]
-
-
 # Creamos la figura
 fig = plt.figure()
 # Agrrgamos un plano 3D
 ax1 = fig.gca(projection='3d')
 # Mostramos el gráfico
-ax1.scatter3D(u, u2, u3, c='g', marker='o')
+ax1.scatter3D(num[:-2], num[1:-1], num[2:], c='g', marker='o')
 plt.show()
 
-
-#x[0] = 4857964
-u2[0] = x[0]/2**31
 
